fa659b49b80346ce8bc4162084426e2ce74a8435
Bilal <bilalmuhammadyousuf543@gmail.com>
2023-09-14 23:07:14 +0500

    change toggle icon

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
             <nav className="navbar navbar-expand-lg bg-success">
                 <div className="container-fluid">
                     <a className="navbar-brand fw-bold text-light" href="/">🛒 TechMart.pk</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon bg-light"></span>
+                    <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
